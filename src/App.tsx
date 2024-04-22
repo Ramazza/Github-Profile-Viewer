a0b98ddf8e7e0b0ae64f5e90868a1fdf5ed91192
PecorinaAmaga01! <01ramazza@gmail.com>
2024-04-22 10:53:44 -0300

    add react-router to other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import Repository from './pages/repo';
+import Profile from './pages/profile';
+import Followers from './pages/followers';
+import Following from './pages/following';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/profile' element={<Profile />} />		
+        <Route path='/repo' element={<Repository />} />		
+        <Route path='/followers' element={<Followers />} />		
+        <Route path='/following' element={<Following />} />		
+      </Routes>
+		</BrowserRouter>
   );
 }
 
