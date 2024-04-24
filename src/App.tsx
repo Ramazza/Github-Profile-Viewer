c38e0216349f0f3c6e949e12a1c7bbbe00f6e02c
PecorinaAmaga01! <01ramazza@gmail.com>
2024-04-24 12:09:38 -0300

    Add route to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Profile from './pages/profile';
+import Repos from './pages/repos';
+import Follower from './pages/followers';
+import Following from './pages/following';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/profile' element={<Profile />} />		
+        <Route path='/repos' element={<Repos />} />		
+        <Route path='/followers' element={<Follower />} />		
+        <Route path='/following' element={<Following />} />		
       </Routes>
 		</BrowserRouter>
   );
